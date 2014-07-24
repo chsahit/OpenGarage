f9797f87afb93dd73fa283481bf1b5f4986f985e
root <root@beaglebone.(none)>
2014-07-24 16:44:37 +0000

    Set up config system for customization

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import Adafruit_BBIO.GPIO as GPIO
 import simplejson
 import requests
+import config
 input = "P8_12"
 def main():
 	last_state = 0
 	GPIO.setup(input,GPIO.IN)
-	url = 'https://api.sendhub.com/v1/messages/?username=9088874698&api_key=2b2b92905ca9711e267610e98ed71da3c2b96359'
+	url = 'https://api.sendhub.com/v1/messages/?username=%s&api_key=%s' %(config.username,config.api_key)
 	payload = {
-		"contacts" : ["+19088874698","+19086194012"],
+		"contacts" : config.contacts,
 		"text": "status change"
 	}
 	headers = {"content-type" : "application/json"}
@@ -16,9 +17,10 @@ def main():
 		if curr_state != last_state:
 			print "status change"
 			if curr_state == 1:
-				payload["text"] = "garage opened"
+				payload["text"] = config.message_pushed
 			else :
-				payload["text"] = "garage closed"
+				payload["text"] = config.message_released
+			print url
 			requests.post(url,data=simplejson.dumps(payload),headers=headers)
 		last_state = curr_state
 
